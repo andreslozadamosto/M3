66f8bbd208c66ca46c1ed55a0ac0c6e8e874eae9
Andres Lozada Mosto <alfathenus@gmail.com>
2014-04-14 16:26:40 -0300

    readme file update and adding api documentation

---
+++ api/api.js
@@ -0,0 +1,55 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AreaChart",
+        "Axis",
+        "ColumnChart",
+        "DonutChart",
+        "LineChart",
+        "M3.AbstractCartesianChart",
+        "M3.AbstractChart",
+        "M3.AbstractPolarChart",
+        "M3.Axis.AxisItem",
+        "M3.Axis.AxisManager",
+        "M3.Axis.CategoryAxis",
+        "M3.Axis.CategoryPointsAxis",
+        "M3.Axis.LinealAxis",
+        "M3.Axis.Stacked100Axis",
+        "M3.Axis.StackedAxis",
+        "M3.Chart",
+        "M3.DataViz",
+        "M3.Series.AreaSerie",
+        "M3.Series.ChartSerie",
+        "M3.Series.ColumnSerie",
+        "M3.Series.GroupColumnSerie",
+        "M3.Series.StackedAreaSerie",
+        "M3.Series.StackedSerie",
+        "PieChart",
+        "Series",
+        "StackedAreaChart",
+        "StackedColumnChart"
+    ],
+    "modules": [
+        "M3",
+        "M3.Axis",
+        "M3.Series"
+    ],
+    "allModules": [
+        {
+            "displayName": "M3",
+            "name": "M3",
+            "description": "Base class for Charts"
+        },
+        {
+            "displayName": "M3.Axis",
+            "name": "M3.Axis",
+            "description": "Module for axis"
+        },
+        {
+            "displayName": "M3.Series",
+            "name": "M3.Series",
+            "description": "Series Module"
+        }
+    ]
+} };
+});
