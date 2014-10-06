682973986346ce9b423ff7f6461a9bbe24ccd8cc
Andres Lozada Mosto <alfathenus@gmail.com>
2014-10-05 21:10:49 -0300

    remove older version files

--- tests/core/AbstractChartSpec.js
+++
@@ -1,8 +0,0 @@
-describe("AbstractChart test", function() {
-    "use strict";
-    
-    it("M3.AbstractChart reference exist", function() {
-        //var a = undefined;
-        expect(M3.AbstractChart).toBeDefined();
-    });
-});
--- themes/darktalker/assets/darktalker/helpers.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by JetBrains PhpStorm.
- * User: darktalker
- * Date: 10-12-26
- * Time: 下午3:44
- * To change this template use File | Settings | File Templates.
- */
-dojo.provide("Darktalker.helpers");
-dojo.require("Darktalker.helpers._base");
